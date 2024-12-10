5c1f53c787007ca89a7be9882a5baba0525b0299
Nazim Boudeffa <boudeffa@gmail.com>
2024-12-10 22:31:37 +0100

    added tip

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import logo from "@/assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import tip from "@/assets/tipeee_tip_btn.svg";
 
 export default function Navbar() {
   return (
@@ -11,9 +12,22 @@ export default function Navbar() {
           <Image src={logo} width={40} height={40} alt="Your Job Board logo" />
           <span className="text-xl font-bold tracking-tight">Your Job Board</span>
         </Link>
+        <div className="flex gap-3">
         <Button asChild>
           <Link href="/jobs/new">Post a job</Link>
         </Button>
+        <Link
+            href='https://fr.tipeee.com/nazimboudeffa'
+            className='cursor-pointer rounded text-brand-orange'
+        >
+            <Image
+                src={tip}
+                alt="tip"
+                height={68}
+                width={68} 
+            />
+        </Link>
+        </div>
       </nav>
     </header>
   );
