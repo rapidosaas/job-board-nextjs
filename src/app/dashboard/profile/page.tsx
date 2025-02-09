05821079b430121de693224e4e6e758625479250
Salim Benfarhat <hello@salim.link>
2025-02-09 22:11:45 +0100

    fix issue #36

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -12,7 +12,7 @@ import {
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 import { ProfileValues, profileSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -22,107 +22,108 @@ import SkillsInput from "@/components/SkillsInput";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
+import { avatars } from "@/lib/avatars";
 
 import Profile from "@/lib/types/profile";
 
-const randomImages = [
-    { name: avatar1, source: "avatar-1.png" },
-    { name: avatar2, source: "avatar-2.png" },
-    { name: avatar3, source: "avatar-3.png" },
-];
+const availableAvatars = avatars;
 
 export default function ProfileForm() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    if (!session) {
-        redirect("/");
-    }
+  if (!session) {
+    redirect("/");
+  }
 
-    const [defaultValues, setDefaultValues] = useState<Profile | undefined>({} as Profile);
-    const [usernameError, setUsernameError] = useState("");
+  const [defaultValues, setDefaultValues] = useState<Profile | undefined>(
+    {} as Profile
+  );
+  const [usernameError, setUsernameError] = useState("");
 
-    const form = useForm<ProfileValues>({
-        defaultValues,
-        resolver: zodResolver(profileSchema),
-    });
+  const form = useForm<ProfileValues>({
+    defaultValues,
+    resolver: zodResolver(profileSchema),
+  });
 
-    const {
-        handleSubmit,
-        control,
-        setFocus,
-        reset,
+  const {
+    handleSubmit,
+    control,
+    setFocus,
+    reset,
     formState: { isSubmitting },
-    } = form;
+  } = form;
 
-    useEffect(() => {
-        if (session) {
-            // Fetch the user's profile data
-            fetch("/api/dashboard/profile", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Data:', data);
-                const profileData = {
-                  ...data.profile
-                };
-                setDefaultValues(profileData);
-                reset(profileData);
-            });
-        } else {
-            redirect("/");
-        }
-    }, [session, reset]);
+  useEffect(() => {
+    if (session) {
+      // Fetch the user's profile data
+      fetch("/api/dashboard/profile", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Data:", data);
+          const profileData = {
+            ...data.profile,
+          };
+          setDefaultValues(profileData);
+          reset(profileData);
+        });
+    } else {
+      redirect("/");
+    }
+  }, [session, reset]);
 
-    console.log('Default Values:', defaultValues);
+  console.log("Default Values:", defaultValues);
 
+  const checkUsernameAvailability = async (
+    username: string,
+    userId: string | undefined
+  ) => {
+    const response = await fetch("/api/dashboard/check-username", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, userId }),
+    });
+    const data = await response.json();
+    return data.isAvailable;
+  };
 
-    const checkUsernameAvailability = async (username: string, userId: string | undefined) => {
-      const response = await fetch("/api/dashboard/check-username", {
+  async function onSubmit(values: ProfileValues) {
+    const isAvailable = await checkUsernameAvailability(
+      values.username,
+      session?.user?.id
+    );
+    if (!isAvailable) {
+      setUsernameError("Username is already taken");
+      return;
+    } else {
+      setUsernameError("");
+    }
+    try {
+      await fetch("/api/dashboard/profile", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, userId }),
+        body: JSON.stringify({
+          ...values,
+          userId: session?.user?.id,
+        }),
       });
-      const data = await response.json();
-      return data.isAvailable;
-    };
-
-    async function onSubmit(values: ProfileValues) {
-        const isAvailable = await checkUsernameAvailability(values.username, session?.user?.id);
-        if (!isAvailable) {
-          setUsernameError('Username is already taken');
-          return;
-        } else {
-          setUsernameError("");
-        }
-        try {
-            await fetch("/api/dashboard/profile", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    ...values,
-                    userId: session?.user?.id,
-                }),
-            });
-        } catch (error) {
-            console.error("Error saving profile:", error);
-        }
+    } catch (error) {
+      console.error("Error saving profile:", error);
     }
-
-    const availableAvatars = randomImages;
+  }
 
   return (
     <main className="m-auto my-10 max-w-4xl space-y-10">
       <div className="space-y-5 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Profile</h1>
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Profile
+        </h1>
         <p className="text-muted-foreground">
           Get your profile seen by thousands of companies.
         </p>
@@ -143,9 +144,12 @@ export default function ProfileForm() {
                   <FormControl>
                     <Input
                       {...field}
-                      value={field.value ?? ''}
+                      value={field.value ?? ""}
                       onChange={(e) => {
-                        const value = e.target.value.replace(/[^a-zA-Z0-9]/g, '');
+                        const value = e.target.value.replace(
+                          /[^a-zA-Z0-9]/g,
+                          ""
+                        );
                         field.onChange(value);
                       }}
                     />
@@ -161,7 +165,7 @@ export default function ProfileForm() {
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input {...field} value={field.value ?? ''}/>
+                    <Input {...field} value={field.value ?? ""} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -174,7 +178,7 @@ export default function ProfileForm() {
                 <FormItem>
                   <FormLabel>Skills</FormLabel>
                   <FormControl>
-                    <SkillsInput 
+                    <SkillsInput
                       onLocationSelected={field.onChange}
                       ref={field.ref}
                       knownskills={defaultValues?.skills}
@@ -192,15 +196,15 @@ export default function ProfileForm() {
                   <FormLabel>Daily rate</FormLabel>
                   <FormControl>
                     <Input
-                    {...field}
-                    type="number"
-                    value={field.value ?? 0}
-                    onChange={(e) => {
-                      const value = e.target.value;
-                      if (value === "" || Number(value) >= 0) {
-                        field.onChange(value === "" ? "" : Number(value));
-                      }
-                    }}
+                      {...field}
+                      type="number"
+                      value={field.value ?? 0}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        if (value === "" || Number(value) >= 0) {
+                          field.onChange(value === "" ? "" : Number(value));
+                        }
+                      }}
                     />
                   </FormControl>
                   <FormMessage />
@@ -212,9 +216,7 @@ export default function ProfileForm() {
               name="bio"
               render={({ field }) => (
                 <FormItem>
-                  <Label onClick={() => setFocus("bio")}>
-                    Bio
-                  </Label>
+                  <Label onClick={() => setFocus("bio")}>Bio</Label>
                   <FormControl>
                     <Textarea
                       placeholder="Tell us a little bit about yourself"
@@ -227,44 +229,46 @@ export default function ProfileForm() {
               )}
             />
             <FormField
-                control={form.control}
-                name="image"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Avatar</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                                onValueChange={(e: string) => {
-                                    field.onChange(e);
-                                }}
-                                defaultValue={field.value}
-                                value={field.value}
-                                className="flex flex-row flex-wrap gap-2 max-xl:justify-center"
-                            >
-                                {availableAvatars.map((image) => (
-                                    <FormItem key={image.name.src}>
-                                        <FormLabel className="[&:has([data-state=checked])>img]:border-primary [&:has([data-state=checked])>img]:border-1 [&:has([data-state=checked])>img]:p-px cursor-pointer">
-                                            <FormControl>
-                                                <RadioGroupItem
-                                                    value={image.source}
-                                                    className="sr-only"
-                                                />
-                                            </FormControl>
+              control={form.control}
+              name="image"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Avatar</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={(e: string) => {
+                        field.onChange(e);
+                      }}
+                      defaultValue={field.value}
+                      value={field.value}
+                      className="flex flex-row flex-wrap gap-2 max-xl:justify-center"
+                    >
+                      {availableAvatars.map((avatar) => (
+                        <FormItem key={avatar.source}>
+                          <FormLabel className="[&:has([data-state=checked])>img]:border-primary [&:has([data-state=checked])>img]:border-1 [&:has([data-state=checked])>img]:p-px cursor-pointer">
+                            <FormControl>
+                              <RadioGroupItem
+                                value={avatar.source}
+                                className="sr-only"
+                              />
+                            </FormControl>
 
-                                            <Image
-                                                key={image.name.src}
-                                                src={image.name}
-                                                alt="avatar"
-                                                className="h-12 w-12 rounded-full border hover:p-px hover:border-primary transition-transform"
-                                            />
-                                        </FormLabel>
-                                    </FormItem>
-                                ))}
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+                            <Image
+                              key={avatar.source}
+                              src={avatar.image}
+                              alt={avatar.name}
+                              width={48}
+                              height={48}
+                              className="h-12 w-12 rounded-full border hover:p-px hover:border-primary transition-transform"
+                            />
+                          </FormLabel>
+                        </FormItem>
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <LoadingButton type="submit" loading={isSubmitting}>
               Save
