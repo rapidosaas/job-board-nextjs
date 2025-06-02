c973de35158d17f4b3288d75148ec589cf50f219
Nazim Boudeffa <boudeffa@gmail.com>
2025-06-02 18:40:01 +0200

    added a better profile

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { formatMoney } from "@/lib/helpers";
-import { Banknote } from "lucide-react";
 import { useEffect, useState } from "react";
 import Profile from "@/lib/types/profile";
+import Image from "next/image";
+
+import avatar1 from "@/assets/avatar-1.png";
+import avatar2 from "@/assets/avatar-2.png";
+import avatar3 from "@/assets/avatar-3.png";
+
+const randomImages = [
+  { name: avatar1, source: "avatar-1.png" },
+  { name: avatar2, source: "avatar-2.png" },
+  { name: avatar3, source: "avatar-3.png" },
+];
 
 interface JobPageProps {
   readonly username: string;
@@ -35,25 +45,29 @@ export default function JobPage({
       });
   }, [username]);
 
+  const getAvatarSource = (avatar: string | null) => {
+    console.log('Avatar:', avatar);
+    const avatarObj = randomImages.find((img) => img.source === avatar);
+    console.log('Avatar:', avatarObj);
+    return avatarObj ? avatarObj.name.src : avatar1.src;
+  };
+
   if (loading) return <div className="p-4 text-center">Loading profile...</div>;
   if (error) return <div className="p-4 text-center text-red-600">{error}</div>;
 
   return (
-    <section className="w-full grow space-y-5">
-      <div className="flex items-center gap-3">
-        <div>
-          <div>
-            <h1 className="text-xl font-bold">{profile?.username}</h1>
-            <p className="font-semibold">
-                <span>{profile?.name}</span>
-            </p>
-          </div>
-          <div className="text-muted-foreground">
-            <p className="flex items-center gap-1.5">
-              <Banknote size={16} className="shrink-0" />
-              {formatMoney(profile?.salary ?? 0)} 
-            </p>
-            <p className="flex items-center gap-1">
+    <section className="grid gap-6 max-w-[400px] md:max-w-[500px] mx-auto">
+      <div className="rounded-2xl border p-10 text-center shadow-lg">
+        <Image 
+          src={getAvatarSource(profile?.image ?? null)}
+          alt={`${profile?.name}'s avatar`}
+          width={100}
+          height={100}
+          className="mx-auto mb-4 h-24 w-24 rounded-full object-cover"        
+        />
+        <h1 className="text-xl font-semibold text-slate-800">{profile?.name}</h1>
+        <h2 className="font-semibold text-slate-500">{formatMoney(profile?.salary ?? 0)}</h2>
+        <p className="inline-block">
               {profile?.skills.map((skill: string) => (
                 <span
                   key={skill}
@@ -63,10 +77,10 @@ export default function JobPage({
                 </span>
               ))}
             </p>
-          </div>
-        </div>
+        <p className="mt-8 text-sm font-normal text-slate-800">
+          {profile?.bio ?? "This user has not provided a bio yet."}
+        </p>
       </div>
-      <div>{profile?.bio}</div>
     </section>
   );
 }
