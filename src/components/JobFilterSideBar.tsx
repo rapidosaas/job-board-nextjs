72b3b550d76d614eada092c6c87818408ca5dd86
Nazim Boudeffa <boudeffa@gmail.com>
2025-06-02 15:43:05 +0200

    little fix

--- src/components/JobFilterSideBar.tsx
+++ src/components/JobFilterSideBar.tsx
@@ -39,7 +39,7 @@ export default function JobFilterSidebar({
 }: Readonly<JobFilterSidebarProps>) {
 
   return (
-    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
+    <aside className="md:sticky md:top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px] w-full mb-4 md:mb-0">
       <form action={filterJobs} key={JSON.stringify(defaultValues)}>
         <div className="space-y-4">
           <div className="flex flex-col gap-2">
